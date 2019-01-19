30123ba0d7a38c5f13c7a8a093476c4e17f2c042
lichongjia <lichongjia@vip.qq.com>
2019-01-19 15:41:49 +0800

    no message

--- my-js/data-loader-engine.js
+++ my-js/data-loader-engine.js
@@ -48,9 +48,9 @@ function data_loader_engine(contentBlocks,xml) {
 						var item = $(value).text()
 						
 						if (!!contentInfo.reverseRender) {
-							$(key).prepend( $(contentInfo.paragraphElement).html("<p>"+item+"</p>") )
+							$(key).prepend( $(contentInfo.paragraphElement).html(item) )
 						} else {
-							$(key).append( $(contentInfo.paragraphElement).html("<p>"+item+"</p>") )
+							$(key).append( $(contentInfo.paragraphElement).html(item) )
 						}
 					});
 				}
